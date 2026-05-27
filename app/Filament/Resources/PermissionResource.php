ecd7047c7fecce42b358b0e497cf45608eb8b47a
Super User <superuser@gmail.com>
2026-05-27 15:27:47 +0700

    Fix PermissionResource table actions syntax

--- app/Filament/Resources/PermissionResource.php
+++ app/Filament/Resources/PermissionResource.php
@@ -66,14 +66,12 @@ class PermissionResource extends Resource
             ->filters([
                 //
             ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+            ->recordActions([
+                \Filament\Actions\EditAction::make(),
+                \Filament\Actions\DeleteAction::make(),
             ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+            ->groupedBulkActions([
+                \Filament\Actions\DeleteBulkAction::make(),
             ]);
     }
 
